7764119cf05b68d20ba0fdddf8f118f997679e82
jDLSUStudent <j_ilustrisimo@dlsu.edu.ph>
2021-01-19 03:30:29 +0800

    building nCheckWinner

--- TicTacToe.c
+++ TicTacToe.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int nValidateMove();
+int nCheckWinner();
+
 void main() 
 {
     int nCol = 0, nRow = 0, nIndex1 = 0, nIndex2 = 0, nPlayer = 0, nWinner = 0, nValidMove = 0, i = 0, j = 0;
@@ -71,6 +73,37 @@ void main()
                     && (cBoard[0][j]=='X' || cBoard[0][j]=='O'))) /*Vertical*/                
 					nWinner++; 
     }
+}
+
+int nCheckWinner(char cBoard[5][5])
+{
+    int nWinner = 0, j = 0;
+
+    if((cBoard[0][0]==cBoard[1][1] 
+        && cBoard[0][0]==cBoard[2][2] 
+        && cBoard[0][0]==cBoard[3][3] 
+        && cBoard[0][0]==cBoard[4][4]) 
+        || (cBoard[0][4]==cBoard[1][3] 
+        && cBoard[0][4]==cBoard[2][2] 
+        && cBoard[0][4]==cBoard[3][1] 
+        && cBoard[0][4]==cBoard[4][0])) /*Diagonal*/
+        nWinner++;
+    else
+        for(j = 0; j <= 4; j++)  //need to set j = 0 in for-loop condition
+            if((cBoard[j][0]==cBoard[j][1] 
+                && cBoard[j][0]==cBoard[j][2]
+                && cBoard[j][0]==cBoard[j][3] 
+                && cBoard[j][0]==cBoard[j][4]
+                && (cBoard[j][0]=='X' || cBoard[j][0]=='O')) /*Horizontal*/
+                || (cBoard[0][j]==cBoard[1][j] 
+                && cBoard[0][j]==cBoard[2][j]
+                && cBoard[0][j]==cBoard[3][j] 
+                && cBoard[0][j]==cBoard[4][j]
+                && (cBoard[0][j]=='X' || cBoard[0][j]=='O'))) /*Vertical*/                
+                nWinner++;
+                
+    return nWinner;
+
 }
 
 int nValidateMove(int nRow,int nCol,int nBoardIndex)
