734342fb553652c42af315ff8bc0d40898a93dc2
jDLSUStudent <j_ilustrisimo@dlsu.edu.ph>
2021-01-31 19:58:19 +0800

    built extraTurn()

--- TicTacToe.c
+++ TicTacToe.c
@@ -19,8 +19,7 @@ void playerAbilities(int nAbilities[PLAYERS][CHOICES], int nPlayer);
 /*
     SPECIAL ABILITIES FUNCTIONS
 */
-//void extraTurn(char cToken, char cBoard[5][5], int nPlayerMove[2], void (*move)(int *, char *, char), void (*input)(int *, char));
-void extraTurn(int i);
+void extraTurn(char cToken, char cBoard[5][5], int nPlayerMove[2], void (*move)(int *, char *, char), void (*input)(int *, char), int (*valid)(int, int, int));
 void steal(char cBoard[5][5], char cToken);
 void cleanCorners(char cBoard[5][5]);
 void rowWipe(char cBoard[5][5], char cToken);
@@ -45,10 +44,10 @@ void main()
     /*Run game*/
 
     //nChooseAbilities(nAbilities, printAbilities);
-    for (size_t d = 0; d < 4; d++)
+    /*for (size_t d = 0; d < 4; d++)
     {
         cBoard[4][d] = 'O';
-    }
+    }*/
 
     for (; i < 25 && !nCheckWinner(cBoard); i++)
     {
@@ -57,10 +56,10 @@ void main()
         /*set cToken*/
         cToken = (nPlayer(i) == 1) ? 'X' : 'O';
 
-        if(i == 2)
+        /*if(i == 2)
         {
-            extraTurn(i);
-        }
+            extraTurn(cToken, cBoard, nPlayerMove, move, askInput, nValidateMove);
+        }*/
         do
         {   /*set cBoard indices from input*/
             askInput(nPlayerMove, cToken);
@@ -222,9 +221,18 @@ void playerAbilities(int nAbilities[PLAYERS][CHOICES], int nPlayer)
 /*  
     SPECIAL ABILITIES FUNCTIONS' DEFINITIONS
 */
-void extraTurn(int i)
+void extraTurn(char cToken, char cBoard[5][5], int nPlayerMove[2], void (*move)(int *, char *, char), void (*input)(int *, char), int (*valid)(int, int, int))
 {
-    --i;
+    printf("Extra Turn:\n");
+
+    do
+    {
+        input(nPlayerMove, cToken);
+
+    } while (!valid(nPlayerMove[0], nPlayerMove[1], cBoard[--nPlayerMove[0]][--nPlayerMove[1]]));
+
+    move(nPlayerMove, cBoard, cToken);
+    
 }
 
 void steal(char cBoard[5][5], char cToken)
